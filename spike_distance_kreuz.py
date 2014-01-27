902e6e407e8fff229da13f00c48c590e23d415fe
Achilleas <Achilleas@tessa>
2014-01-27 13:25:52 +0200

    Code formatting

--- spike_distance_kreuz.py
+++ spike_distance_kreuz.py
@@ -1,5 +1,3 @@
-#!/bin/env python
-
 '''
 
 Comment by Thomas Kreuz:
@@ -16,16 +14,14 @@ J Neurophysiol 109, 1457-1472 (2013).
 The Matlab codes as well as more information can be found at http://www.fi.isc.cnr.it/users/thomas.kreuz/sourcecode.html.
 
 '''
-
-
-
 import numpy as np
 
-'''
-Return the times (t1,t2) of the spikes in train[ibegin:]
-such that t1 < t and t2 >= t
-'''
+
 def find_corner_spikes(t, train, ibegin, ti, te):
+    '''
+    Return the times (t1,t2) of the spikes in train[ibegin:]
+    such that t1 < t and t2 >= t
+    '''
     if(ibegin == 0):
         tprev = ti
     else:
@@ -59,8 +55,12 @@ def bivariate_spike_distance(t1, t2, ti, te, N):
     ibegin_t2 = 0
     corner_spikes[:,0] = t
     for itc, tc in enumerate(t):
-       corner_spikes[itc,1:3], ibegin_t1 = find_corner_spikes(tc, t1, ibegin_t1, ti, te)
-       corner_spikes[itc,3:5], ibegin_t2 = find_corner_spikes(tc, t2, ibegin_t2, ti, te)
+       corner_spikes[itc,1:3], ibegin_t1 = find_corner_spikes(tc, t1,
+                                                              ibegin_t1,
+                                                              ti, te)
+       corner_spikes[itc,3:5], ibegin_t2 = find_corner_spikes(tc, t2,
+                                                              ibegin_t2,
+                                                              ti, te)
 
     #print corner_spikes
     xisi = np.zeros((N,2))
@@ -68,16 +68,29 @@ def bivariate_spike_distance(t1, t2, ti, te, N):
     xisi[:,1] = corner_spikes[:,4] - corner_spikes[:,3]
     norm_xisi = np.sum(xisi,axis=1)**2.0
 
-    # We now compute the smallest distance between the spikes in t2 and the corner spikes of t1
+    # We now compute the smallest distance between the spikes in t2
+    # and the corner spikes of t1
     # with np.tile(t2,(N,1)) we build a matrix :
-    # np.tile(t2,(N,1)) =    [   t2   ]        -   np.tile(reshape(corner_spikes,(N,1)), t2.size) = [                        ]
-    #                        [   t2   ]                                                             [  corner  corner  corner]
-    #                        [   t2   ]                                                             [                        ]
-    dp1 = np.min(np.fabs(np.tile(t2,(N,1)) - np.tile(np.reshape(corner_spikes[:,1],(N,1)),t2.size)),axis=1)
-    df1 = np.min(np.fabs(np.tile(t2,(N,1)) - np.tile(np.reshape(corner_spikes[:,2],(N,1)),t2.size)),axis=1)
+    # np.tile(t2,(N,1)) = [t2] - np.tile(reshape(corner_spikes,(N,1)), t2.size) = [                        ]
+    #                     [t2]                                                    [  corner  corner  corner]
+    #                     [t2]                                                    [                        ]
+    dp1 = np.min(np.fabs(np.tile(t2,(N,1))
+                         - np.tile(np.reshape(corner_spikes[:,1],(N,1)),
+                                   t2.size)),
+                 axis=1)
+    df1 = np.min(np.fabs(np.tile(t2,(N,1))
+                         - np.tile(np.reshape(corner_spikes[:,2],(N,1)),
+                                   t2.size)),
+                 axis=1)
     # And the smallest distance between the spikes in t1 and the corner spikes of t2
-    dp2 = np.min(np.fabs(np.tile(t1,(N,1)) - np.tile(np.reshape(corner_spikes[:,3],(N,1)),t1.size)),axis=1)
-    df2 = np.min(np.fabs(np.tile(t1,(N,1)) - np.tile(np.reshape(corner_spikes[:,4],(N,1)),t1.size)),axis=1)
+    dp2 = np.min(np.fabs(np.tile(t1,(N,1))
+                         - np.tile(np.reshape(corner_spikes[:,3],
+                                              (N,1)),t1.size)),
+                 axis=1)
+    df2 = np.min(np.fabs(np.tile(t1,(N,1))
+                         - np.tile(np.reshape(corner_spikes[:,4],(N,1)),
+                                   t1.size)),
+                 axis=1)
 
     xp1 = t - corner_spikes[:,1]
     xf1 = corner_spikes[:,2] - t
@@ -91,13 +104,16 @@ def bivariate_spike_distance(t1, t2, ti, te, N):
 
     return t,d
 
+
 def multivariate_spike_distance(spike_trains, ti, te, N):
-    ''' t is an array of spike time arrays
+    '''
+    t is an array of spike time arrays
     ti the initial time of the recordings
     te the end time of the recordings
     N the number of samples used to compute the distance
     spike_trains is a list of arrays of shape (N, T) with N spike trains
-    The multivariate distance is the instantaneous average over all the pairwise distances
+    The multivariate distance is the instantaneous average over all the
+    pairwise distances
     '''
     d = np.zeros((N,))
     n_trains = len(spike_trains)
@@ -110,6 +126,7 @@ def multivariate_spike_distance(spike_trains, ti, te, N):
             d = d + dij
     d = d / float(n_trains * (n_trains-1) /2)
     return t,d
+
 
 if __name__ == '__main__':
     import matplotlib.pylab as plt
