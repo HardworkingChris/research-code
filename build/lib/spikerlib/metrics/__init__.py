709a67438a5f8570ad104946764db239e4ad9ba1
Achilleas <Achilleas@Morgana>
2014-02-01 21:26:15 +0200

    spikerlib egg stuff

---
+++ build/lib/spikerlib/metrics/__init__.py
@@ -0,0 +1,12 @@
+"""
+Spike train distance metrics. Includes code for calculating
+some of the most widely used spike time similarity metrics
+(Victor-Purpura, Kreuz SPIKE-distance, Florian modulus metric).
+
+TODO: List functions and classes and finish docstrings.
+"""
+import corrcoef
+import kreuz
+import victor_purpura
+import modulus_metric
+
