8af7320403d366a325865ec551f8c8d7b46e4f34
Achilleas <achilleas.k@gmail.com>
2014-06-10 19:14:25 +0300

    New spkerlib egg (0.7)

--- spikerlib_setup.py
+++ spikerlib_setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="spikerlib",
-      version="0.4",
+      version="0.7",
       description="Collection of tools for analysing spike trains",
       author="Achilleas Koutsou",
       author_email="achilleas.k@gmail.com",
