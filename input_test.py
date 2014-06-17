f33e93cea34b4782b1f0e154f27f2fdf7b0d8502
Achilleas <achilleas.k@gmail.com>
2014-06-17 19:01:01 +0300

    Testing fast synchronous input generator

--- input_test.py
+++ input_test.py
@@ -1,17 +1,21 @@
 from brian import *
-from spikerlib.tools import SynchronousInputGroup
+from spikerlib.tools import fast_synchronous_input_gen
 
 dura = 0.5*second
 r = 100*Hz
-s = 1
-j = 1*ms
+s = 0.7
+j = 0.5*ms
+j = 0
 n = 20
 
-print("Constructing spike generator ...")
-inp_gen = SynchronousInputGroup(n,r,s,j)
-print("Setting up SpikeGeneratorGroup ...")
-inp = SpikeGeneratorGroup(n, inp_gen())
+#print("Constructing spike generator ...")
+#inp_gen = SynchronousInputGroup(n,r,s,j)
+#print("Setting up SpikeGeneratorGroup ...")
+#inp = SpikeGeneratorGroup(n, inp_gen())
+
+inp = fast_synchronous_input_gen(n, r, s, j, dura)
 
 inp_mon = SpikeMonitor(inp)
-run(dura, report='stdout')
+network = Network(inp, inp_mon)
+network.run(dura, report='stdout')
 
