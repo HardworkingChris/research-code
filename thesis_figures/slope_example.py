5813effa6be2772209c83c1094deb05bfede1ec1
Achilleas <achilleas.k@gmail.com>
2015-04-08 21:05:47 +0300

    Basic simulation working

--- thesis_figures/slope_example.py
+++ thesis_figures/slope_example.py
@@ -1,8 +1,11 @@
-from brian import (Network, NeuronGroup, StateMonitor,
-                   mV, ms)
+from brian import (Network, NeuronGroup, StateMonitor, SpikeMonitor,
+                   Connection,
+                   mV, ms, Hz)
+import matplotlib.pyplot as plt
 import spikerlib as sl
 
 sim = Network()
+duration = 200*ms
 tau = 10*ms
 Vth = 15*mV
 Vreset = 0*mV
@@ -12,6 +15,24 @@ lifnrn = NeuronGroup(1, lifeq, threshold="V>=Vth", reset=Vreset)
 lifnrn.V = Vreset
 sim.add(lifnrn)
 
-vmon = StateMonitor(lifnrn, "V", record=True)
-sim.add(vmon)
+Nin = 200
+fin = 20*Hz
+Sin = 0.3
+sigma = 1*ms
+weight = 0.2*mV
+inputs = sl.tools.fast_synchronous_input_gen(Nin, fin, Sin, sigma, duration)
+connection = Connection(inputs, lifnrn, "V", weight=weight)
+sim.add(inputs, connection)
 
+vmon = StateMonitor(lifnrn, "V", record=True)
+spikemon = SpikeMonitor(lifnrn)
+sim.add(vmon, spikemon)
+
+sim.run(duration)
+vmon.insert_spikes(spikemon, 40*mV)
+
+plt.figure()
+plt.plot(vmon.times*1000, vmon[0]*1000)
+plt.xlabel("t (ms)")
+plt.ylabel("Membrane potential (mV)")
+plt.show()
