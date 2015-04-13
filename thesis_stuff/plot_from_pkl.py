85b9603e9aa3a878f84c23338689fe9d523b6630
Achilleas <achilleas.k@gmail.com>
2015-04-14 01:11:32 +0300

    Filter results with no refractory period and plot for all interpolations

--- thesis_stuff/plot_from_pkl.py
+++ thesis_stuff/plot_from_pkl.py
@@ -10,8 +10,21 @@ try:
 except IOError:
     filedata = {}
 
+interpolations = ["none", "nearest", "bilinear", "bicubic", "spline16",
+                  "spline36", "hanning", "hamming", "hermite", "kaiser",
+                  "quadric", "catrom", "gaussian", "bessel", "mitchell",
+                  "sinc", "lanczos"]
+
 for k in filedata.keys():
     nrndeftuple, Nin, weight, syncconf, fout, Vth, tau = k
+    # skip simulations that had no refractory period
+    refr = False
+    for item in nrndeftuple:
+        if "refractory" in item:
+            refr = True
+    if not refr:
+        continue
+    print(nrndeftuple)
     allnpss = filedata[k]
     mnpss = np.array([np.mean(an) for an in allnpss])
     Sin = [sc[0] for sc in syncconf]
@@ -22,13 +35,16 @@ for k in filedata.keys():
     imextent = (0, 1, 0, 4.0)
     mnpss = np.reshape(mnpss, imshape, order="F")
     plt.figure()
-    plt.imshow(mnpss, aspect="auto", origin="lower", extent=imextent,
-               interpolation="none", vmin=0, vmax=1)
-    cbar = plt.colorbar()
-    cbar.set_label("$\overline{M}$")
-    plt.xlabel("$S_{in}$")
-    plt.ylabel("$\sigma_{in}$ (ms)")
-    filename = "npss_{}_{}_{}".format(Nin, weight, fout).replace(".", "")
-    plt.savefig(filename+".pdf")
-    plt.savefig(filename+".png")
-    print("{} saved".format(filename))
+    for interpol in interpolations:
+        plt.clf()
+        plt.imshow(mnpss, aspect="auto", origin="lower", extent=imextent,
+                   interpolation=interpol, vmin=0, vmax=1)
+        cbar = plt.colorbar()
+        cbar.set_label("$\overline{M}$")
+        plt.xlabel("$S_{in}$")
+        plt.ylabel("$\sigma_{in}$ (ms)")
+        filename = "npss_{}_{}_{}_{}".format(Nin, weight, fout,
+                                             interpol).replace(".", "")
+        plt.savefig(filename+".pdf")
+        plt.savefig(filename+".png")
+        print("{} saved".format(filename))
