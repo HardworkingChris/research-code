9a95391b91d97e414dd367b9cf9bab765c1b0232
Achilleas <achilleas.k@gmail.com>
2015-04-14 02:29:17 +0300

    Plotting correlation figures for edges

--- thesis_stuff/plot_from_pkl.py
+++ thesis_stuff/plot_from_pkl.py
@@ -28,13 +28,38 @@ for k in filedata.keys():
     allnpss = filedata[k]
     mnpss = np.array([np.mean(an) for an in allnpss])
     Sin = [sc[0] for sc in syncconf]
-    Sin = set(Sin)
-    sigma = [sc[1] for sc in syncconf]
-    sigma = set(sigma)
+    Sin = sorted(list(set(Sin)))
+    sigma = [sc[1]*1000 for sc in syncconf]
+    sigma = sorted(list(set(sigma)))
     imshape = (len(sigma), len(Sin))
-    imextent = (0, 1, 0, 4.0)
+    syncconf = [{"S": s, "J": j} for s in Sin for j in sigma]
+    syncconf = np.reshape(syncconf, imshape, order="F")
     mnpss = np.reshape(mnpss, imshape, order="F")
-    plt.figure()
+    imextent = (0, 1, 0, 4.0)
+
+    # PLOTTING
+    plt.figure("Sin vs M")
+    plt.clf()
+    plt.plot(Sin, mnpss[0], "b--", marker=".", markersize=15)
+    plt.plot([0, 1], [0, 1], "k--")
+    plt.xlabel("$S_{in}$")
+    plt.ylabel("$\overline{M}$")
+    filename = "SyncM_{}_{}_{}".format(Nin, weight, fout).replace(".", "")
+    plt.savefig(filename+".pdf")
+    plt.savefig(filename+".png")
+    print("{} saved".format(filename))
+
+    plt.figure("Sigma vs M")
+    plt.clf()
+    plt.plot(sigma, mnpss[:,-1], "b--", marker=".", markersize=15)
+    plt.plot([0, 4], [1, 0], "k--")
+    plt.xlabel("$\sigma_{in}$ (ms)")
+    plt.ylabel("$\overline{M}$")
+    filename = "SigmM_{}_{}_{}".format(Nin, weight, fout).replace(".", "")
+    plt.savefig(filename+".pdf")
+    plt.savefig(filename+".png")
+    print("{} saved".format(filename))
+    plt.figure("NPSS")
     for interpol in interpolations:
         plt.clf()
         plt.imshow(mnpss, aspect="auto", origin="lower", extent=imextent,
