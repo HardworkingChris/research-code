49fb400dfef5a350271a3642a93928b2aec2a17e
Achilleas <achilleas.k@gmail.com>
2015-04-14 06:54:58 +0300

    Some image formatting for thesis

--- thesis_stuff/plot_from_pkl.py
+++ thesis_stuff/plot_from_pkl.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-NPSS_FILE = "npssresults.pkl"
+NPSS_FILE = "pkl/npssresults.pkl"
 
 try:
     with open(NPSS_FILE, 'r') as npssfile:
@@ -10,10 +10,11 @@ try:
 except IOError:
     filedata = {}
 
-interpolations = ["none", "nearest", "bilinear", "bicubic", "spline16",
-                  "spline36", "hanning", "hamming", "hermite", "kaiser",
-                  "quadric", "catrom", "gaussian", "bessel", "mitchell",
-                  "sinc", "lanczos"]
+# interpolations = ["none", "nearest", "bilinear", "bicubic", "spline16",
+#                   "spline36", "hanning", "hamming", "hermite", "kaiser",
+#                   "quadric", "catrom", "gaussian", "bessel", "mitchell",
+#                   "sinc", "lanczos"]
+interpolations = ["hamming"]
 
 for k in filedata.keys():
     nrndeftuple, Nin, weight, syncconf, fout, Vth, tau = k
@@ -38,38 +39,48 @@ for k in filedata.keys():
     imextent = (0, 1, 0, 4.0)
 
     # PLOTTING
-    plt.figure("Sin vs M")
+    plt.figure("Sin vs M", figsize=(4,3))
     plt.clf()
     plt.plot(Sin, mnpss[0], "b--", marker=".", markersize=15)
     plt.plot([0, 1], [0, 1], "k--")
     plt.xlabel("$S_{in}$")
     plt.ylabel("$\overline{M}$")
-    filename = "SyncM_{}_{}_{}".format(Nin, weight, fout).replace(".", "")
+    plt.subplots_adjust(bottom=0.15, top=0.95, left=0.2, right=0.95)
+    filename = "figures/SyncM_{}_{}_{}".format(Nin, weight, fout).replace(".", "")
     plt.savefig(filename+".pdf")
     plt.savefig(filename+".png")
     print("{} saved".format(filename))
 
-    plt.figure("Sigma vs M")
+    plt.figure("Sigma vs M", figsize=(4,3))
     plt.clf()
     plt.plot(sigma, mnpss[:,-1], "b--", marker=".", markersize=15)
     plt.plot([0, 4], [1, 0], "k--")
+    plt.xticks([0, 1, 2, 3, 4])
+    ylocs, ylabels = plt.yticks()
+    plt.yticks(ylocs, [])
     plt.xlabel("$\sigma_{in}$ (ms)")
-    plt.ylabel("$\overline{M}$")
-    filename = "SigmM_{}_{}_{}".format(Nin, weight, fout).replace(".", "")
+    # plt.ylabel("$\overline{M}$")
+    plt.subplots_adjust(bottom=0.15, top=0.95, left=0.2, right=0.95)
+    filename = "figures/SigmM_{}_{}_{}".format(Nin, weight, fout).replace(".", "")
     plt.savefig(filename+".pdf")
     plt.savefig(filename+".png")
     print("{} saved".format(filename))
-    plt.figure("NPSS")
+    plt.figure("NPSS", figsize=(4,3))
     for interpol in interpolations:
         plt.clf()
         plt.imshow(mnpss, aspect="auto", origin="lower", extent=imextent,
                    interpolation=interpol, vmin=0, vmax=1)
         cbar = plt.colorbar()
         cbar.set_label("$\overline{M}$")
+        cbar.set_ticks([0, 0.25, 0.5, 0.75, 1.0])
+        plt.xticks([0, 0.5, 1.0])
+        plt.yticks([0, 1.0, 2.0, 3.0, 4.0])
         plt.xlabel("$S_{in}$")
         plt.ylabel("$\sigma_{in}$ (ms)")
-        filename = "npss_{}_{}_{}_{}".format(Nin, weight, fout,
-                                             interpol).replace(".", "")
+        filename = "figures/npss_{}_{}_{}_{}".format(Nin, weight, fout,
+                                                     interpol).replace(".", "")
+
+        plt.subplots_adjust(bottom=0.25, top=0.95, left=0.2, right=0.8)
         plt.savefig(filename+".pdf")
         plt.savefig(filename+".png")
         print("{} saved".format(filename))
